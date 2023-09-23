a17cadbd9e150d704a70e8e6a2952cdef3bc3d57
Дякин Антон Андреевич <dyakin.aa@dev.local>
2023-09-23 12:59:58 +0300

    init

---
+++ src/test/java/logic/ApplicationTest.java
@@ -0,0 +1,13 @@
+package logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+}
